6a2cc62b827ebdd278a3f9b2f9f594be5bd07222
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-06-08 17:54:43 +0200

    fixed bugs in task controller - added vscode debugger configurations

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,36 +1,52 @@
 const Task = require('../models/task.model');
 
 // Main task: utility function
-const imageGenerator = async Group => {
-  const foundImage = await Task.findOne({ Group });
-
-  if (foundImage) return false;
-  else return true;
+const imageGenerator = async group => {
+  try {
+    const foundTask = await Task.findOne({ group });
+    if (foundTask) return false;
+    else return true;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // Main task: Request handler for generating image
 exports.generateImage = async (req, res) => {
-  const { Group } = req.body;
-  const foundImage = await imageGenerator(Group);
-  if (foundImage)
-    return res
-      .status(400)
-      .json({ message: 'an image of these students already exists' });
-  else res.status(200).json({ message: 'image successfully generated' });
+  try {
+    const { group } = req.body;
+    const notFoundImage = await imageGenerator(group);
+    if (notFoundImage)
+      return res.status(200).json({ message: 'image successfully generated' });
+    else
+      return res
+        .status(400)
+        .json({ message: 'an image of these students already exists' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'internal server error' });
+  }
 };
 
-// Alternative solution: Generate image during task creation
+/////////////
+///////////// Alternative solution: Generate image during task creation
 // without the need of another endpoint to check in the past generated images
 exports.createTask = async (req, res) => {
-  const { Number, Group, Class } = req.body;
+  try {
+    const { number, group, class: schoolClass } = req.body;
 
-  // Generate image in a separate function that either
-  // creates the image and return the path if successfully generated (true)
-  // or respond with already generated, and the path of the past generated image (false)
-  const hasGeneratedNewImage = await imageGenerator(Group);
+    // Generate image in a separate function that either
+    // creates the image and return the path if successfully generated (true)
+    // or respond with already generated, and the path of the past generated image (false)
+    const hasGeneratedNewImage = await imageGenerator(group);
 
-  const result = await Task.create({ Number, Class, Group });
-  return res
-    .status(201)
-    .json({ student: result, 'has-generated-new-image': hasGeneratedNewImage });
+    const result = await Task.create({ number, class: schoolClass, group });
+    return res.status(201).json({
+      student: result,
+      'has-generated-new-image': hasGeneratedNewImage
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'internal server error' });
+  }
 };
--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: `./.${process.env.NODE_ENV}.env` });
-const app = require('express')();
+const express = require('express');
+const app = express();
 const tasksRouter = require('./routers/task.routes');
+
+app.use(express.json());
 
 app.use('/tasks', tasksRouter);
 
